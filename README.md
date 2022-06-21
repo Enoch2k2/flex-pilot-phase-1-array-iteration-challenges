405955543f1810488a8b6619b820313bd8bd6476
Enoch2k2 <enoch2k2@hotmail.com>
2022-06-21 08:24:26 -0500

    array iteration challenges

---
+++ README.md
@@ -0,0 +1,7 @@
+# Array Iteration Challenges
+
+Follow the guidelines listed in index.js (as comments) in order to complete each function. To run the code to make sure you get the expected results type in the terminal:
+
+```
+  node index.js
+```
---
+++ index.js
@@ -0,0 +1,116 @@
+/*
+
+  Search Function:
+
+   should be able to search for anything inside a string, for example. If I was to pass in "o", it should log:
+
+   1. Bob
+   2. Collins
+   3. Spongebob
+
+   Should log a human readable list, see how you can add an index to the iteration
+
+   Hint: Good possability that two iterators are used here, also use the data array above for this.
+
+*/
+
+const data = [
+  "Bob",
+  "Reya",
+  "Phil",
+  "Collins",
+  "Leah",
+  "Skywalker",
+  "Spongebob",
+  "Marissa",
+  "Kyle",
+  "Lisa"
+]
+
+
+function search(term) {
+
+}
+
+search("o")
+
+/*
+
+  switch S for $ function:
+
+    This function should give us back a modified array where any s inside the array of names is transformed into a $ sign. Also should be logged back in a humanb readable list. Result should be:
+
+    1. "Bob",
+    2. "Reya",
+    3. "Phil",
+    4. "Collin$",
+    5. "Leah",
+    6. "$kywalker",
+    7. "$pongebob",
+    8. "Mari$$a",
+    9. "Kyle",
+    10. "Li$a"
+
+    Note: Capitalization shouldn't matter here, also use the data array above for this
+*/
+
+function switchSfor$() {
+
+}
+
+switchSfor$();
+
+/*
+  Iterate through an array of objects in order to find a person that matches the id passed in! If no person in the array exist, log out, oops didn't find anyone! If it does exist, then log, Found <person's name>!
+
+  Ex:
+
+  findPerson(1)
+
+  logs => Found Samuel!
+
+  findPerson(100)
+
+  logs => oops didn't find anyone!
+*/
+
+const arrayOfObjects([
+  { id: 1, name: "Samuel" },
+  { id: 2, name: "Rhea" },
+  { id: 3, name: "Linda" },
+  { id: 4, name: "Clarissa" },
+  { id: 5, name: "Bob" },
+  { id: 6, name: "Jojo" },
+  { id: 7, name: "Parker" }
+])
+
+function findPerson(id) {
+  // iterate through arrayOfObjects here
+}
+
+
+findPerson(1)
+findPerson(100)
+
+
+/*
+  calculateTheCost:
+
+    Iterate through the list of items using the reduce function in order to find the cost of the items listed below. Should log the result: The price is 2324!
+*/
+
+const items = [
+  { name: "phone", price: 899 },
+  { name: "tv", price: 1399 },
+  { name: "soap", price: 5 },
+  { name: "lightbulb", price: 6 },
+  { name: "shampoo", price: 6 },
+  { name: "cheese", price: 5 },
+  { name: "tomatoes", price: 4 }
+]
+
+function calculateTheCost() {
+
+}
+
+calculateTheCost();
